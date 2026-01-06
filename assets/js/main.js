2c43fbb1a37997d900bd2be29bc572595fc76aa3
arielrosenmann94 <hola@arielrosenmann.com>
2026-01-06 20:03:50 -0300

    primer commit - en la calse 1 de git y github

---
+++ assets/js/main.js
@@ -0,0 +1,113 @@
+//alert("OJO esto es una alerta!!!!")
+
+    //confirm("¿Está seguro de eliminar este elemento?");
+
+    ////prompt("escribe algo:")
+//
+    //let x = "11";
+//
+    //console.log("¿Queé tipo de dato es la variable x?");
+    //console.log(typeof x);
+//
+    //x = parseInt(x);
+    //console.log("¿Queé tipo de dato es la variable x?");
+    //console.log(typeof x);
+//
+    //let y = 5;
+//
+    //let suma = x + y;
+    //console.log(suma);
+    //let nombre = "Juan";
+    //let apellido = "Perez";
+//
+    //let concatenarDiferente = `Hola ${nombre} ${apellido}!`;
+    //let concatenar = "Hola" + " " + nombre + " " + apellido + "!";
+    //console.log(concatenarDiferente);
+    //console.log(concatenar);
+//
+//
+    //let rut = "18123456";
+    //let digitiVerificador = "9";
+//
+    //let sumaRut = rut + "-" + digitiVerificador;
+    //console.log(sumaRut);
+    ////let multiplicacion = x + y * suma;
+//
+   ////console.log(multiplicacion)
+//
+//
+    //let multiplicacion = x * y;
+//
+    //console.log(multiplicacion);
+//
+    //let division = x / y ;
+//
+    //console.log(division);
+//
+    //let resto = x % y;
+//
+    //console.log(resto);
+//
+    //let resta = x - y;
+    //console.log(resta)
+//
+    //let exponente = x ** y;
+    //console.log(exponente)
+//
+//
+    //let nombreCompleto = "Tu nombre completo";
+    //
+    //let nombrePrimerApellido = "";
+
+
+
+    let a = 1;
+
+    let b = 1;
+
+    let sumar = a + 1;
+    a = a + 1;
+
+    b = b - 1; //0
+
+    b--; //-1
+
+    b++; // 0
+    
+    b = b + 1; // +1
+
+    b += 10; // +11
+
+    b -= 6; // +5
+
+    b *= 2; // +10
+
+    b /= 2;// +5
+
+    b %= 2; // +1
+
+    b = b / 2 ;
+    //console.log(b);
+
+    let o = 5;
+    let u = "5";
+
+    //console.log( o == u); // ¿Es igual el caracter?
+    //console.log( o != u); // ¿Es difernete el caracter?
+
+    let p = 5; //int
+    let g = 5; //str
+//
+//    //console.log( y === g); // ¿Es  igual el caracter o el tipo de dato?
+    //console.log( y !== g); // ¿Es diferente el caracter o el tipo de dato?
+
+
+   //console.log(p == 5 || g > 10 ); 
+
+
+let hora = 12;
+
+if(hora<12){console.log("Buenos días");
+} else {
+ console.log("Buenas tardes");
+}
